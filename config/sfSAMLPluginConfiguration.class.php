2240eff6c8533a9da87d2c145766c912ba6804d5
chtitux <chtitux@6d8ba722-8b52-4e9a-913e-53f6b10cf52b>
2010-08-04 13:02:34 +0000

    Correction LICENCE block
    git-svn-id: http://saml.chtitux.org/svn/saml/trunk/plugins/sfSAMLPlugin@69 6d8ba722-8b52-4e9a-913e-53f6b10cf52b

--- config/sfSAMLPluginConfiguration.class.php
+++ config/sfSAMLPluginConfiguration.class.php
@@ -1,8 +1,9 @@
 <?php
 
 /*
- * This file is part of the symfony package.
- * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ * This file is part of the sfSAMLplugin package.
+ * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
+ * (c) 2010      Théophile Helleboid <t.helleboid@iariss.fr>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
